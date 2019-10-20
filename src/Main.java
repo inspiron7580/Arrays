d26811313355ffc1b1e21c1cf6df8c89b4627f85
redian23 <vasya.degski@gmail.com>
2019-10-20 13:32:17 +0300

    Create rep Arrays

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] integer_array = new int [10];
+        integer_array[0]=0;
+        integer_array[1]=2;
+        integer_array[2]=4;
+        integer_array[3]=6;
+        integer_array[4]=8;
+        integer_array[5]=10;
+        System.out.println(integer_array.length);
+
+        String[] strings_array = new String[20];
+        for (int i = 0; i < 10; i++) {
+            System.out.println(strings_array.length);
+        }
+    }
+}
